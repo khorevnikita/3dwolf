b321d4a213f09e548624fdcc9d519a20ac5a4668
nikita <tulula.nk@gmail.com>
2023-05-21 15:35:00 -0500

    fix text

--- backend/resources/views/emails/invitation.blade.php
+++ backend/resources/views/emails/invitation.blade.php
@@ -1,3 +1,7 @@
 <div>
-    {{var_dump($user->toArray())}}: {{$password}}
+    <h1>Привет, {{$user->name}}</h1>
+    <p>Добро пожаловать в {{config("app.name")}}!</p>
+    <p>Логин для входа: <b>{{$user->email}}</b></p>
+    <p>Пароль: <b>{{$password}}</b></p>
+    <a href="{{url("/")}}">Войти</a>
 </div>
--- backend/resources/views/emails/reset.blade.php
+++ backend/resources/views/emails/reset.blade.php
@@ -1,3 +1,7 @@
 <div>
-    {{var_dump($user->toArray())}}: {{$password}}
+    <h1>Привет, {{$user->name}}</h1>
+    <p>Вам сбросили пароль от {{config("app.name")}}!</p>
+    <p>Логин для входа: <b>{{$user->email}}</b></p>
+    <p>Новый пароль: <b>{{$password}}</b></p>
+    <a href="{{url("/")}}">Войти</a>
 </div>
