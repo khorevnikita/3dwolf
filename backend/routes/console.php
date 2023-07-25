7495f8ab5253b31d36026c89d4594ef90e2b18e7
nikita <tulula.nk@gmail.com>
2023-07-25 13:59:55 +0300

    add randomize

--- backend/routes/console.php
+++ backend/routes/console.php
@@ -17,3 +17,29 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('randomize', function () {
+    $customers = \App\Models\Customer::query()->get()->each(function (\App\Models\Customer $customer) {
+        $faker = Faker\Factory::create('ru_RU');
+        $name = $faker->firstName('male');
+        $surname = $faker->lastName('male');
+        $phone = $faker->phoneNumber();
+        $email = $faker->safeEmail();
+        $customer->fill([
+            'name' => $name, 'surname' => $surname, 'phone' => $phone, 'email' => $email,
+        ]);
+        $customer->save();
+        if ($user = $customer->user()->first()) {
+            $user->fill([
+                'name' => $name, 'surname' => $surname, 'phone' => $phone, 'email' => $email,
+            ]);
+            $user->save();
+        }
+        $customer->orders()->get()->each(function (\App\Models\Order $order) use ($phone) {
+            $order->fill([
+                'phone' => $phone,
+            ]);
+            $order->save();
+        });
+    });
+})->purpose('Display an inspiring quote');
